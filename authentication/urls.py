df1063e2e3f5e86d391a5f3ca2b2ffa3b409df7a
soumiguria <guriasoumi29@gmail.com>
2023-04-21 10:12:15 +0530

    loginPage

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('signup', views.signup, name="signup"),
+    path('activate/<uidb64>/<token>', views.activate, name='activate'),
     path('signin', views.signin, name="signin"),
     path('signout', views.signout, name="signout"),
 ]
